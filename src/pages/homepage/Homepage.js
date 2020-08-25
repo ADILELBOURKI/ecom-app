b5d9e978bede3150ba9e5493ba5ee4c4d07ef337
ADILELBOURKI <adilbcsm@gmail.com>
2020-08-25 13:18:39 +0100

    splitting the project into components (separation of concern)

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.scss";
+import Wrapper from "../../components/wrapper/Wrapper";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Wrapper />
+    </div>
+  );
+};
+
+export default HomePage;
